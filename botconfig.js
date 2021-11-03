39484698cf6f7221ab9c5ec1a4426d6d41c2dced
lukman-nov <novawiyadi@gmail.com>
2021-11-04 00:14:47 +0700

    Kucluck Pro v3.0.5

--- botconfig.js
+++ botconfig.js
@@ -6,17 +6,17 @@ module.exports = {
   Token: "OTAxNDQxODMwNjQwNTU0MDA0.YXP7IA.kI_wvpI-LQ9IaaFCRrM8bNIAGdI", //Discord Bot Token
   ClientID: "901441830640554004", //Discord Client ID
   ClientSecret: "Ot27tkg5Z4IRQ24WOVYwFK1Fz_7pzpSN", //Discord Client Secret
-  Admins: ["377636455016824834"], //Admins of the bot
-  ExpressServer: true, //If you wanted to make the website run or not
-  DefaultPrefix: ".", //Default prefix, Server Admins can change the prefix
-  Port: 4000, //Which port website gonna be hosted
-  SupportServer: "https://discord.gg/BuACxn4XUw", //Support Server Link
-  Scopes: ["identify", "guilds", "applications.commands"], //Discord OAuth2 Scopes
-  ServerDeafen: false, //If you want bot to stay deafened
-  DefaultVolume: 100, //Sets the default volume of the bot, You can change this number anywhere from 1 to 100
-  CallbackURL: "/api/callback", //Discord OAuth2 Callback URL
-  "24/7": true, //If you want the bot to be stay in the vc 24/7
-  CookieSecret: "Pikachu is cute", //A Secret like a password
+  Admins: ["377636455016824834"],
+  ExpressServer: true, 
+  DefaultPrefix: "!!", //Default prefix, Server Admins can change the prefix
+  Port: 4000, 
+  SupportServer: "https://discord.gg/BuACxn4XUw",
+  Scopes: ["identify", "guilds", "applications.commands"], 
+  ServerDeafen: true,
+  DefaultVolume: 100,
+  CallbackURL: "/api/callback", 
+  "24/7": true,
+  CookieSecret: "Pikachu is cute", 
   IconURL:
     "https://cdn.discordapp.com/attachments/898516000604061699/904998671781867530/Logo-pro-small.gif", //URL of all embed author icons | Dont edit unless you dont need that Music CD Spining
   EmbedColor: "RANDOM", //Color of most embeds | Dont edit unless you want a specific color instead of a random one each time
