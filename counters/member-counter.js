73edf165e8704a4dbf1a49905cf5648e8a213886
lukman-nov <novawiyadi@gmail.com>
2021-11-04 00:11:40 +0700

    Kucluck Pro v3.0.4

--- counters/member-counter.js
+++ counters/member-counter.js
@@ -1,8 +1,8 @@
 module.exports = async (client) =>{
-    let guild = client.guilds.cache.get('898516000604061696');
+    let guild = client.guilds.cache.get('901445288881963059');
     setInterval(() =>{
         let memberCount = guild.memberCount;
-        let channel = guild.channels.cache.get('902140586490994708');
+        let channel = guild.channels.cache.get('901770869251129364');
         channel.setName(`Total Members: ${memberCount.toLocaleString()}`);
         console.log('Updating Member Count');
     }, 100000);
